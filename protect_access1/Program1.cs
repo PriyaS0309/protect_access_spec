e43c5af6233bd505353429a381de720109ba8e2b
PriyaS0309 <109306451+PriyaS0309@users.noreply.github.com>
2022-07-25 16:53:44 +0530

    Add project files.

---
+++ protect_access1/Program1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace protect_access1
+{
+    public class ABC
+    {
+        protected void sample()
+        {
+            Console.WriteLine("This the protected class");
+        }
+    }
+ 
+    class Program1 :ABC                                              
+    {
+
+        static void Main(string[] args)
+        {
+            Program1 p = new Program1();                                        
+            p.sample();                                            //protected members are accessed in derived class
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ protect_access2/Program2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using protect_access1;
+
+namespace protect_access2
+{
+    class Program2:ABC                            //protect members of class ABC can be accessed on derived class of outside the assebly
+    {
+        static void Main(string[] args)
+        {
+            Program2 p1 = new Program2();              
+            p1.sample();
+        }
+    }
+}
